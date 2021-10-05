43968eb199c056ea910df84fa86464e6136af817
AlexisBeltran <jesus_beltran011@outlook.com>
2021-10-05 16:25:46 -0600

    controllers and public area added

--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,11 @@
 <?php 
     require_once __DIR__ . "/../includes/app.php";
-    use MVC\Router;
-    
+    use MVC\Router;    
+    use Controllers\paginaControllers;
     $Router = new Router();
 
+    //PUBLIC AREA
+    $Router->get('/', [paginaControllers::class, 'index']);
+
+    $Router->ComprobarRutas();
 ?>
