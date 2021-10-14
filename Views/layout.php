f3a47d6280dc41c057e06cc6c14a2d3febea0979
aristeoibarra <aristeoibarra608@gmail.com>
2021-10-14 12:45:58 -0600

    Test_environment deploy added

--- Views/layout.php
+++ Views/layout.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Krispy Antojo</title>
 </head>
+
 <body>
     <header>
     </header>
-    <?php echo $Contenido;?>
+    <?php echo $Contenido; ?>
     <footer>
     </footer>
+    <script id="ic-api" src="https://T60f8f4ef5a3f5800084b0b42.projects.ideascloud.io/api.js"></script>
 </body>
+
 </html>
