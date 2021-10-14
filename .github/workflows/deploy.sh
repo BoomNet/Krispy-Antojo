dcf3e6d35d38044f192b86d60cd2b15d2e9c2abc
aristeoibarra <aristeoibarra608@gmail.com>
2021-10-14 13:04:47 -0600

    compile_gulp fixed

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,4 +5,4 @@ IC_CUSTOM_WEBAPP_ID=$3
 npm install
 gulp
 sudo npm i -g ideascloud-cli
-IC_PROJECT_ACCESS_TOKEN=IcCCDUbPsOGT6bzsD2QZBWAe6AgraaWNn99MYbbvn7vdVBqBQF ic-cli project 60f8f4ef5a3f5800084b0b42 upload-custom-webapp-version --custom-webapp-id=616877aa6c6f470008f846ab --description="Version from cli" --folder-path=public/build
+IC_PROJECT_ACCESS_TOKEN=IcCCDUbPsOGT6bzsD2QZBWAe6AgraaWNn99MYbbvn7vdVBqBQF ic-cli project 60f8f4ef5a3f5800084b0b42 upload-custom-webapp-version --custom-webapp-id=616877aa6c6f470008f846ab --description="Version from cli" --folder-path=Public/build
