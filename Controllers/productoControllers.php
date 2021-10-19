ce678fa39e181e965ba280587f60b87add3d4633
AlexisBeltran <jesus_beltran011@outlook.com>
2021-10-19 15:51:01 -0600

    productoControllers crud producto added

--- Controllers/productoControllers.php
+++ Controllers/productoControllers.php
@@ -2,4 +2,46 @@
     namespace Controllers;
     use MVC\Router;
     use Model\Producto;
+
+    class productoControllers{
+        public static function crear(Router $router){
+            $producto = new Producto;
+            $Errores = Producto::getError();
+            if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                $producto = new Producto($_POST['producto']);
+                $Errores = $producto->validarProducto();
+                if(empty($Errores)){
+                    $producto->Guardar();
+                }
+            }
+            $router->render('producto/crear', [
+                'producto' => $producto,
+                'Errores' => $Errores
+            ]);
+        }
+
+        public static function actualizar(Router $router){
+            $id = ValidarORedireccionar('/admin');
+            $Errores = Producto::getError();
+            $producto = Producto::find($id);            
+            if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                $args = new Producto($_POST['producto']);
+                $producto->Sincronizar($args);
+                $Errores = $producto->validarRol();
+                if(empty($Errores)){
+                    $producto->Guardar();
+                }
+            }
+            $router->render('/producto/actualizar');            
+        }
+
+        public static function Eliminar(){
+            $id = $_POST['id'];
+            $id = filter_var($id, FILTER_VALIDATE_INT);
+            if($id){
+                $producto = Producto::find($id);
+                $producto->eliminar();
+            }
+        }
+    }
 ?>
