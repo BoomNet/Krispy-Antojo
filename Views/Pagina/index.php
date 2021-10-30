3f7a32059e96c5fe8374f336c0390d028d5ac7ab
RolandoOsuna <60458339+RolandoOsuna@users.noreply.github.com>
2021-10-29 23:30:52 -0600

    Primer Vista (login) (#13)

--- Views/Pagina/index.php
+++ Views/Pagina/index.php
@@ -1,1 +1,24 @@
-<h1>TEST</h1>
+<div  class="text-center bg-login ">
+    <main class="container">
+        <div class="row  justify-content-center">
+            <div class="col-12 col-md-6 col-lg-4">
+                <form class="bg-white  p-3 rounded-p shadow" style="height: 550px;">
+                    <img class="img-fluid py-3" src="/IMG/Logo-login.png" alt="">
+                    <h1 class="h4 mb-3">Iniciar Sesión</h1>
+                    <!-- Correo -->
+                    <div class="col-12 form-floating px-3">
+                        <input type="text" class="form-ctrl " id="correo" placeholder="Correo">
+                    </div>
+                    <!-- Contraseña -->
+                    <div class="col-12 form-floating px-3 mt-3">
+                        <input type="password" id="password" class="form-ctrl" placeholder="Contraseña">
+                    </div>
+                    <button type="submit" class="w-75 button btn-lg btn-rosa rounded-pill text-center my-4">Iniciar Sesión</button>
+                    <div  class="col-12 align-self-end pt-5">
+                        <p class="form-text pt-5">¿No tienes una cuenta? <a href="signup.php" class="text-decoration-none">Registrate</a> </p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>    
+</div>
--- Views/layout.php
+++ Views/layout.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Krispy Antojo</title>
     <link href="/build/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/build/css/login.css" rel="stylesheet" >
 </head>
 <body>
     <header>
