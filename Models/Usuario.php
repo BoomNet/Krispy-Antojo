bacd77bb3e2aa5939d2c1003130e1d9cb9641634
AlexisBeltran <jesus_beltran011@outlook.com>
2021-11-04 14:55:32 -0700

    Function validarLogin added

--- Models/Usuario.php
+++ Models/Usuario.php
@@ -4,15 +4,16 @@
     class Usuario extends ActiveRecord{
 
         protected static $Tabla = "usuario";
-        protected static $ColumnaDB = ['cve_usuario', 'nombre_usuario', 'apellidopa_usuario', 'apellidoma_usuario', 'telefono_usuario', 'usuario_usuario', 'contraseña_usuario', 'fechacrea_usuario', 'fechamod_usuario', 'cverol_usuario'];
+        protected static $ColumnaDB = ['cve_usuario', 'nombre_usuario', 'apellidopa_usuario', 'apellidoma_usuario', 'correo_usuario', 'telefono_usuario', 'usuario_usuario', 'contrasenia_usuario', 'fechacrea_usuario', 'fechamod_usuario', 'cverol_usuario'];
 
         public $cve_usuario;
         public $nombre_usuario;
         public $apellidopa_usuario;
         public $apellidoma_usuario;
+        public $correo_usuario;
         public $telefono_usuario;
         public $usuario_usuario;
-        public $contraseña_usuario;
+        public $contrasenia_usuario;
         public $fechacrea_usuario;
         public $fechamod_usuario;
         public $cverol_usuario;
@@ -22,9 +23,10 @@
             $this->nombre_usuario = $args['nombre_usuario'] ?? '';
             $this->apellidopa_usuario = $args['apellidopa_usuario'] ?? '';
             $this->apellidoma_usuario = $args['apellidoma_usuario'] ?? '';
+            $this->correo_usuario = $args['correo_usuario'] ?? '';
             $this->telefono_usuario = $args['telefono_usuario'] ?? '';
             $this->usuario_usuario = $args['usuario_usuario'] ?? '';
-            $this->contraseña_usuario = $args['contraseña_usuario'] ?? '';
+            $this->contrasenia_usuario = $args['contrasenia_usuario'] ?? '';
             $this->fechacrea_usuario = date('Y/m/d');
             $this->fechamod_usuario = date('Y/m/d') ?? null;
             $this->cverol_usuario = $args['cverol_usuario'] ?? '';
@@ -46,11 +48,20 @@
             if(!$this->usuario_usuario){
                 self::$Errores[] = "El nombre de usuario es obligatorio";
             }
-            if(!$this->contraseña_usuario){
+            if(!$this->contrasenia_usuario){
                 self::$Errores[] = "La contraseña es obligatorio";
             }
             if(!$this->cve_usuario){
                 self::$Errores[] = "El rol es obligatorio";
+            }
+        }
+
+        public function validarLogin(){
+            if(!$this->correo_usuario){
+                self::$Errores[] = "El correo es obligatorio";
+            }
+            if(!$this->contrasenia_usuario){
+                self::$Errores[] = "La contraseña es obligatoria";
             }
         }
         
