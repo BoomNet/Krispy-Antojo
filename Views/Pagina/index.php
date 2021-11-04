3d6457fac9ef0e510133e329397b4bbac6cd080c
Rolando <rolando_osuna@live.com.mx>
2021-11-04 10:20:19 -0700

    Sing in - Log in

--- Views/Pagina/index.php
+++ Views/Pagina/index.php
@@ -1,4 +1,31 @@
-<div class="container">
+<div class="bg-login" style="width: 100%; height: 100vh">
+    <main class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <form class="bg-white  p-3 rounded-p shadow" style="height: 550px;">
+                        <img class="img-fluid ps-4 py-3" src="/build/img/Logo.png" alt="">
+                        <h1 class="h4 mb-3">INICIAR SESION</h1>
+                        <!-- Correo -->
+                        <div class="col-12 form-floating px-3">
+                            <input type="text" class="form-ctrl " id="correo" placeholder="Correo">
+                        </div>
+                        <!-- Contraseña -->
+                        <div class="col-12 form-floating px-3 mt-3">
+                            <input type="password" id="password" class="form-ctrl" placeholder="Contraseña">
+                        </div>
+                        <button type="submit" class="w-75 button btn-lg btn-rosa rounded-pill text-center my-4">INICIAR SESION</button>
+                        <div  class="col-12 align-self-end pt-5">
+                            <p class="form-text pt-5">¿No tienes una cuenta? <a href="/Pagina/crear" class="text-decoration-none">Registrate</a> </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    </main>
+</div>
+
+
+
+<!-- <div class="container">
     <div class="row justify-content-center col">
         <div class="contenido-login">
             <div class="formulario-login">
@@ -9,4 +36,4 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
