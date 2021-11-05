a707e902b0ba3fb00c7c1db65d0d8063f5fc1d2e
AlexisBeltran <jesus_beltran011@outlook.com>
2021-11-05 11:54:14 -0700

    delete comments

--- Views/Pagina/index.php
+++ Views/Pagina/index.php
@@ -38,18 +38,3 @@
         </div>
     </main>
 </section>
-
-
-
-<!-- <div class="container">
-    <div class="row justify-content-center col">
-        <div class="contenido-login">
-            <div class="formulario-login">
-                <div class="pictures">
-                    <img src="/build/img/Logo.png" alt="Logo Krispy">
-                </div>
-                <?php include __DIR__ . "/formulario-login.php"?>
-            </div>
-        </div>
-    </div>
-</div> -->
