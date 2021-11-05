070da728a30a876377e12762a7a1809c185c4bd2
Rolando <rolando_osuna@live.com.mx>
2021-11-05 11:39:59 -0700

    Login terminado

--- Views/Pagina/formulario-crear.php
+++ Views/Pagina/formulario-crear.php
@@ -1,4 +1,39 @@
-<div class="formulario-crear">
+<main class="bg-login" style="width: 100%; height:100vh">
+    <section class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 col-lg-4">
+                <form class="bg-white text-center p-3 rounded-p shadow" style="height: 550px;">
+                    <img class="img-fluid py-3" src="/build/img/Logo.png" alt="">
+                    <h1 class="h4 mb-3">Regístrate</h1>
+                    <!-- Nombre -->
+                    <div class="col-12 form-floating px-3">
+                        <input type="text" class="form-ctrl " id="Nombre" placeholder="Nombre">
+                    </div>
+                    <!-- Correo -->
+                    <div class="col-12 form-floating px-3 mt-3">
+                        <input type="text" class="form-ctrl " id="correo" placeholder="Correo">
+                    </div>
+                    <!-- Contraseña -->
+                    <div class="col-12 form-floating px-3 mt-3">
+                        <input type="password" id="password" class="form-ctrl" placeholder="Contraseña">
+                    </div>
+                    <!--Confirmar Contraseña -->
+                    <div class="col-12 form-floating px-3 mt-3">
+                        <input type="password" id="confirm-password" class="form-ctrl" placeholder="Confirmar Contraseña">
+                    </div>
+                    <button type="submit" class="w-75 button btn-lg btn-rosa rounded-pill text-center my-4">Registrar</button>
+                    <div  class="col-12 pt-5">
+                        <a href="/" class="text-decoration-none form-text link-dark">←Volver</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</main>
+
+
+
+<!-- <div class="formulario-crear">
     <form action="" method="POST">
         <input type="text" placeholder="Nombre" name="usuario_nomre" id="nombre">
         <input type="email" placeholder="Correo Electronico" name="usuario_correo" id="correo">
@@ -7,4 +42,4 @@
         <input type="submit" value="Crear usuario" class="boton boton-rosa">
     </form>
     <a href="/" class="boton boton-rosa">Volver</a>
-</div>
+</div> -->
--- Views/Pagina/index.php
+++ Views/Pagina/index.php
@@ -1,34 +1,43 @@
-<div class="bg-login" style="width: 100%; height: 100vh">
-    <main class="container text-center">
-            <div class="row justify-content-center">
-                <div class="col-12 col-md-6 col-lg-4">
+<section class="bg-login" style="width: 100%; height: 100vh">
+    <main class="container ">
+        <div class="row justify-content-center ">
+            <div class="col-12 col-sm-6 col-md-6 col-lg-4 mt-1">
+                <form class="bg-white text-center flex-column bd-highligh px-3 pt-3 rounded-p shadow" style="height: 550px;" method="POST" action="/">
+                    <img class="img-fluid ps-lg-2 py-3" src="/build/img/Logo.png" alt="">
+                    <h1 class="h4 mb-3">INICIAR SESION</h1>
+                    <!-- Correo -->
                     <?php if($Errores):?>
                         <?php foreach($Errores as $Error):?>
-                            <div class="alerta-error">
-                                <?php echo $Error;?>
+                            <div class="text-start alert alert-danger d-flex bd-highlight " role="alert">
+                                <div class="flex-grow-1 bd-highlight">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
+                                        <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
+                                        <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+                                    </svg>
+                                    <?php echo $Error;?>
+                                </div>
+                                <div class="bd-highlight">
+                                    <button type="button " class="btn-close text-end" data-bs-dismiss="alert"></button>                                    
+                                </div>
                             </div>
                         <?php endforeach;?>
                     <?php endif;?>
-                    <form class="bg-white  p-3 rounded-p shadow" style="height: 550px;" method="POST" action="/">
-                        <img class="img-fluid ps-4 py-3" src="/build/img/Logo.png" alt="">
-                        <h1 class="h4 mb-3">INICIAR SESION</h1>
-                        <!-- Correo -->
-                        <div class="col-12 form-floating px-3">
-                            <input type="text" class="form-ctrl " id="correo" placeholder="Correo" name="correo_usuario">
-                        </div>
-                        <!-- Contraseña -->
-                        <div class="col-12 form-floating px-3 mt-3">
-                            <input type="password" id="password" class="form-ctrl" placeholder="Contraseña" name="contrasenia_usuario">
-                        </div>
-                        <button type="submit" class="w-75 button btn-lg btn-rosa rounded-pill text-center my-4">INICIAR SESION</button>
-                        <div  class="col-12 align-self-end pt-5">
-                            <p class="form-text pt-5">¿No tienes una cuenta? <a href="/Pagina/crear" class="text-decoration-none">Registrate</a> </p>
-                        </div>
-                    </form>
-                </div>
+                    <div class="col-12 form-floating px-3">
+                        <input type="text" class="form-ctrl " id="correo" placeholder="Correo" name="correo_usuario">
+                    </div>
+                    <!-- Contraseña -->
+                    <div class="col-12 form-floating px-3 mt-3">
+                        <input type="password" id="password" class="form-ctrl" placeholder="Contraseña" name="contrasenia_usuario">
+                    </div>
+                    <button type="submit" class="w-75 button btn-lg btn-rosa rounded-pill text-center my-4">INICIAR SESION</button>
+                    <div  class="bd-highligh pt-5">
+                        <p class="form-text pt-5">¿No tienes una cuenta? <a href="/Pagina/formulario-crear" class="text-decoration-none">Registrate</a> </p>
+                    </div>
+                </form>
             </div>
+        </div>
     </main>
-</div>
+</section>
 
 
 
