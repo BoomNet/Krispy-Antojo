4aaba9562c3f0288aeb66b0075a2c0771ee33310
Rolando <rolando_osuna@live.com.mx>
2021-11-13 02:42:54 -0700

    Tabla Usuario - Ventana modal(pendiente)

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -1,71 +1,73 @@
 <div class="sidebar">
-    <div class="logo-details">
-        <div class="logo_name">Krispy Antojo</div>
-        <i class='bx bx-menu' id="btn" ></i>
-    </div>
-    <ul class="nav-list" id="Navegacion">
-      <li>
-          <i class='bx bx-search' ></i>
-         <input type="text" placeholder="Search...">
-         <span class="tooltip">Search</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-grid-alt'></i>
-          <span class="links_name">Dashboard</span>
-        </a>
-         <span class="tooltip">Dashboard</span>
-      </li>
-      <li>
-
-       <a href="/Dashboard/dashboard?View=2">
-         <i class='bx bx-user usuario' ></i>
-         <span class="links_name">Usuario</span>
-       </a>
-       <span class="tooltip">Usuario</span>
-     </li>
-     <li>
-       <a href="#">
-         <i class='bx bx-chat roles'></i>
-         <span class="links_name">Roles</span>
-       </a>
-       <span class="tooltip">Roles</span>
-     </li>
-     <li>
-       <a href="#">
-         <i class='bx bx-pie-chart-alt-2 productos' ></i>
-         <span class="links_name">Productos</span>
-       </a>
-       <span class="tooltip">Productos</span>
-     </li>
-     <li>
-       <a href="#">
-         <i class='bx bx-folder gastos'></i>
-         <span class="links_name">Gasto</span>
-       </a>
-       <span class="tooltip">Gasto</span>
-     </li>
-     <li>
-       <a href="#">
-         <i class='bx bx-cart-alt ventas' ></i>
-         <span class="links_name">Ventas</span>
-       </a>
-       <span class="tooltip">Ventas</span>
-     </li>
-     <li class="profile">
-         <div class="profile-details">
-           <img src="https://avatars.githubusercontent.com/u/46621591?v=4" alt="profileImg">
-           <div class="name_job">
-             <div class="name">Test</div>
-             <div class="job">Web designer</div>
-           </div>
-         </div>
-         <div>
-           <i class='bx bx-log-out logout' id="log_out" ></i>
-         </div>
-     </li>
-    </ul>
+  <div class="logo-details">
+      <div class="logo_name">Krispy Antojo</div>
+      <i class='bx bx-menu' id="btn" ></i>
   </div>
+  <ul class="nav-list" id="Navegacion">
+    <li>
+        <i class='bx bx-search' ></i>
+       <input type="text" placeholder="Search...">
+       <span class="tooltip">Search</span>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-grid-alt'></i>
+        <span class="links_name">Dashboard</span>
+      </a>
+       <span class="tooltip">Dashboard</span>
+    </li>
+    <li>
+
+     <a href="/Dashboard/dashboard?View=2">
+       <i class='bx bx-user usuario' ></i>
+       <span class="links_name">Usuario</span>
+     </a>
+     <span class="tooltip">Usuario</span>
+   </li>
+   <li>
+     <a href="#">
+       <i class='bx bx-chat roles'></i>
+       <span class="links_name">Roles</span>
+     </a>
+     <span class="tooltip">Roles</span>
+   </li>
+   <li>
+     <a href="#">
+       <i class='bx bx-pie-chart-alt-2 productos' ></i>
+       <span class="links_name">Productos</span>
+     </a>
+     <span class="tooltip">Productos</span>
+   </li>
+   <li>
+     <a href="#">
+       <i class='bx bx-folder gastos'></i>
+       <span class="links_name">Gasto</span>
+     </a>
+     <span class="tooltip">Gasto</span>
+   </li>
+   <li>
+     <a href="#">
+       <i class='bx bx-cart-alt ventas' ></i>
+       <span class="links_name">Ventas</span>
+     </a>
+     <span class="tooltip">Ventas</span>
+   </li>
+   <li class="profile">
+       <div class="profile-details">
+         <img src="https://avatars.githubusercontent.com/u/46621591?v=4" alt="profileImg">
+         <div class="name_job">
+           <div class="name">Test</div>
+           <div class="job">Web designer</div>
+         </div>
+       </div>
+       <div>
+         <a href="/Views/Pagina/index.php">
+           <i class='bx bx-log-out logout' id="log_out" ></i>
+         </a>
+       </div>
+   </li>
+  </ul>
+</div>
 <section class="home-section section-main">
   <?php 
     if(isset($View)){
@@ -74,85 +76,15 @@
           include __DIR__ . "/fondo_dashboard.php";
           break;
         case 2:
-          include __DIR__ . "/section_usuario.php";
+          include __DIR__ . "/tabla_usuario.php";
           break;
         default:
           break;
       }
     }
   ?>
-  
-
-         <a href="/Views/Pagina/index.php">
-           <i class='bx bx-log-out' id="log_out" ></i>
-         </a>
      </li>
     </ul>
   </div>
-  <section class="home-section">
-  <div class="bg-light rounded m-5">
-    <h1 class="text-center">Usuario</h1>
-      <form class="row" action="" method="post">
-        <!-- CLAVE-ROL -->
-        <div class="col-12 d-flex px-4">
-          <div class="col-12 col-lg-1 pe-3">
-            <input class="form-control" type="tel" id="cve_usuario" placeholder="Clave">
-          </div>
-          <div class="col-12 col-lg-2">
-            <select class="form-select" id="rol">
-              <option selected>Rol</option>
-              <option value="1">Vendedor</option>
-              <option value="2">Administrador</option>
-            </select>
-          </div>
-        </div>
-        <!-- NOMBRE-APELLIDOS -->
-        <div class="col-12 d-lg-flex">
-          <div class="col-12 col-lg-4 p-1 p-lg-3">
-            <label class="form-label">Nombre</label>
-            <input class="form-control" id="nombre_usuario" type="text">
-          </div>
-          <div class="col-12 col-lg-4 p-1 p-lg-3">
-            <label class="form-label">Apellido Paterno</label>
-            <input class="form-control" id="apellidopa_usuario" type="text">
-          </div>
-          <div class="col-12 col-lg-4 p-1 p-lg-3">
-            <label class="form-label">Apellido Materno</label>
-            <input class="form-control" id="apellidoma_usuario" type="text">
-          </div>
-        </div>
-        <!-- USUARIO-CORREO -->
-        <div class="col-12 d-lg-flex">
-          <div class="col-12 col-lg-4 p-1 p-lg-3">
-            <label class="form-label">Nombre de Usuario</label>
-            <div class="input-group">
-              <div class="input-group-text">@</div>
-              <input class="form-control" id="usuario_usuario" type="text">
-            </div>
-          </div>
-          <div class="col-12 col-lg-8 p-1 p-lg-3">
-            <label class="form-label">Correo</label>
-            <input class="form-control" id="correo" type="email">
-          </div>
-        </div>
-        <!-- CORREO -->
-        <div class="col-12 d-lg-flex">
-          <div class="col-12 col-lg-6 p-1 p-lg-3">
-            <label class="form-label">Contraseña</label>
-            <input class="form-control" id="contrasenia" type="password" >
-          </div>
-          <div class="col-12 col-lg-6 p-1 p-lg-3">
-            <label class="form-label">Confirmar Contraseña</label>
-            <input class="form-control" id="confirm_contrasenia" type="password">
-          </div>
-        </div>
-      </form>
-      <div class="row ">
-        <div class="col-12 text-center p-3">
-          <button class="btn btn-primary" type="button">Registrar</button>
-          <button class="btn btn-secondary" type="button">Cancelar</button>
-        </div>
-      </div>
-    </div>
 
 </section>
--- Views/Dashboard/section_usuario.php
+++ Views/Dashboard/section_usuario.php
@@ -1,8 +1,14 @@
-<div class="bg-light rounded m-5 usuario">
-    <h1 class="text-center">Usuario</h1>
-    <?php if($Errores):?>
-      <?php foreach($Errores as $Error):?>
-        <div class="text-start alert alert-danger d-flex bd-highlight " role="alert">
+<div class="modal fade" id="agg_usuario_modal" data-bs-backdrop="static" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Agregar Usuario</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+      <div class="modal-body">
+      <?php if($Errores):?>
+        <?php foreach($Errores as $Error):?>
+          <div class="text-start alert alert-danger d-flex bd-highlight " role="alert">
           <div class="flex-grow-1 bd-highlight">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
               <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
@@ -14,70 +20,68 @@
             <button type="button " class="btn-close text-end" data-bs-dismiss="alert"></button>                                    
           </div>
         </div>
-      <?php endforeach;?>
-    <?php endif;?>
-    <form class="row" action="/Dashboard/dashboard?View=2" method="POST">
-      <!-- CLAVE-ROL -->
-      <div class="col-12 d-flex px-4">
-        <div class="col-12 col-lg-2">
-          <select class="form-select" name="usuario[cverol_usuario]">
-            <option selected disabled>Seleccione Rol</option>
-            <?php foreach($Rol as $rol):?>
-              <option <?php $usuario->cverol_usuario === $rol->id ? '' : 'selected';?>
-                value="<?php echo $rol->id;?>"><?php echo s($rol->rol);?></option>
-            <?php endforeach;?>
-          </select>
-        </div>
-      </div>
-      <!-- NOMBRE-APELLIDOS -->
-      <div class="col-12 d-lg-flex">
-        <div class="col-12 col-lg-4 p-1 p-lg-3">
-          <label class="form-label">Nombre</label>
-          <input class="form-control" name="usuario[nombre_usuario]" type="text" value=<?php echo $usuario->nombre_usuario;?>>
-        </div>
-        <div class="col-12 col-lg-4 p-1 p-lg-3">
-          <label class="form-label">Apellido Paterno</label>
-          <input class="form-control" name="usuario[apellidopa_usuario]" type="text" value=<?php echo $usuario->apellidopa_usuario;?>>
-        </div>
-        <div class="col-12 col-lg-4 p-1 p-lg-3">
-          <label class="form-label">Apellido Materno</label>
-          <input class="form-control" name="usuario[apellidoma_usuario]" type="text" value=<?php echo $usuario->apellidoma_usuario;?>>
-        </div>
-      </div>
-      <!-- USUARIO-CORREO -->
-      <div class="col-12 d-lg-flex">
-        <div class="col-12 col-lg-4 p-1 p-lg-3">
-          <label class="form-label">Nombre de Usuario</label>
-          <div class="input-group">
-            <div class="input-group-text">@</div>
-            <input class="form-control" name="usuario[usuario_usuario]" type="text" value=<?php echo $usuario->usuario_usuario;?>>
+        <?php endforeach;?>
+      <?php endif;?>
+      <form class="row " action="/Dashboard/dashboard?View=2" method="POST">
+          <!-- CLAVE-ROL -->
+          <div class="col-12 d-flex px-4">
+            <div class="col-12 col-lg-2">
+              <select class="form-select" name="usuario[cverol_usuario]">
+                <option selected disabled>Seleccione Rol</option>
+                <?php foreach($Rol as $rol):?>
+                  <option <?php $usuario->cverol_usuario === $rol->id ? '' : 'selected';?>
+                    value="<?php echo $rol->id;?>"><?php echo s($rol->rol);?></option>
+                <?php endforeach;?>
+              </select>
+            </div>
           </div>
-        </div>
-        <div class="col-12 col-lg-4 p-1 p-lg-3">
-          <label class="form-label">Correo</label>
-          <input class="form-control" name="usuario[correo_usuario]" type="email" value=<?php echo $usuario->correo_usuario;?>>
-        </div>
-        <div class="col-12 col-lg-4 p-1 p-lg-3">
-          <label class="form-label">Telefono:</label>
-          <input class="form-control" name="usuario[telefono_usuario]" type="text" value=<?php echo $usuario->usuario_usuario;?>>
-        </div>
+          <!-- NOMBRE-APELLIDOS -->
+          <div class="col-12 d-lg-flex">
+            <div class="col-12 col-lg-4 p-1 p-lg-3">
+              <label class="">Nombre</label>
+              <input class="form-ctrl" name="usuario[nombre_usuario]" type="text" value=<?php echo $usuario->nombre_usuario;?>>
+            </div>
+            <div class="col-12 col-lg-4 p-1 p-lg-3">
+              <label class="">Apellido Paterno</label>
+              <input class="form-ctrl" name="usuario[apellidopa_usuario]" type="text" value=<?php echo $usuario->apellidopa_usuario;?>>
+            </div>
+            <div class="col-12 col-lg-4 p-1 p-lg-3">
+              <label class="">Apellido Materno</label>
+              <input class="form-ctrl" name="usuario[apellidoma_usuario]" type="text" value=<?php echo $usuario->apellidoma_usuario;?>>
+            </div>
+          </div>
+          <!-- USUARIO-CORREO -->
+          <div class="col-12 d-lg-flex">
+            <div class="col-12 col-lg-4 p-1 p-lg-3">
+              <label class="">Nombre de Usuario</label>
+              <input class="form-ctrl" name="usuario[usuario_usuario]" type="text" placeholder="@" value=<?php echo $usuario->usuario_usuario;?>>
+            </div>
+            <div class="col-12 col-lg-4 p-1 p-lg-3">
+              <label class="">Correo</label>
+              <input class="form-ctrl" name="usuario[correo_usuario]" type="email" value=<?php echo $usuario->correo_usuario;?>>
+            </div>
+            <div class="col-12 col-lg-4 p-1 p-lg-3">
+              <label class="">Telefono:</label>
+              <input class="form-ctrl" name="usuario[telefono_usuario]" type="text" value=<?php echo $usuario->usuario_usuario;?>>
+            </div>
+          </div>
+          <!-- CORREO -->
+          <div class="col-12 d-lg-flex">
+            <div class="col-12 col-lg-6 p-1 p-lg-3">
+              <label class="">Contraseña</label>
+              <input class="form-ctrl" name="usuario[contrasenia_usuario]" type="password" >
+            </div>
+            <div class="col-12 col-lg-6 p-1 p-lg-3">
+              <label class="">Confirmar Contraseña</label>
+              <input class="form-ctrl" name="usuario[confirm_contrasenia]" type="password">
+            </div>
+          </div>
+        </form>
       </div>
-      <!-- CORREO -->
-      <div class="col-12 d-lg-flex">
-        <div class="col-12 col-lg-6 p-1 p-lg-3">
-          <label class="form-label">Contraseña</label>
-          <input class="form-control" name="usuario[contrasenia_usuario]" type="password" >
-        </div>
-        <div class="col-12 col-lg-6 p-1 p-lg-3">
-          <label class="form-label">Confirmar Contraseña</label>
-          <input class="form-control" name="usuario[confirm_contrasenia]" type="password">
-        </div>
-      </div>
-      <div class="row ">
-      <div class="col-12 text-center p-3">
+      <div class="modal-footer">
         <input class="btn btn-primary" type="submit" value="Registrar">
-        <button class="btn btn-secondary" type="button">Cancelar</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
       </div>
     </div>
-    </form>
   </div>
+</div>
