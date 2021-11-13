0cd64151da0e3aaa4fc165e3927857761f9b53b2
AlexisBeltran <jesus_beltran011@outlook.com>
2021-11-12 22:03:29 -0700

    Controller about create user's added

--- Controllers/usuarioControllers.php
+++ Controllers/usuarioControllers.php
@@ -1,23 +1,45 @@
 <?php 
     namespace Controllers;
 
+use Exception;
 use Model\Usuario;
-use MVC\Router;
+    use Model\Rol;
+    use MVC\Router;
 
     class usuarioControllers{
         public static function crear(Router $router){
+            $View = GetView('/Dashboard/dashboard?View=1');
             $usuario = new Usuario;
+            $Rol = Rol::all();
             $Errores = Usuario::getError();
             if($_SERVER['REQUEST_METHOD'] === 'POST'){
                 $usuario = new Usuario($_POST['usuario']);
                 $Errores = $usuario->ValidarUsuario();
                 if(empty($Errores)){
-                    $usuario->Guardar();
+                    $Resultado = $usuario->existeUsuario();
+                    if(!$Resultado){
+                        $Errores = Usuario::getError();
+                    }else{
+                        $comparado = $usuario->CompararPassword();
+                        if($comparado){
+                            $usuario->hashPassword();
+                            unset($usuario->confirm_contrasenia);
+                            $usuario->fechamod_usuario = null;
+                            $guardado = $usuario->Guardar();
+                           if($guardado){
+                               echo "Agregado correctamente";
+                           }
+                        }else{
+                            $Errores = Usuario::getError();
+                        }
+                    }
                 }
             }
-            $router->render('/Pagina/formulario-crear',[
+            $router->render('/Dashboard/dashboard',[
                 'usuario' => $usuario,
-                'Errores' => $Errores
+                'Errores' => $Errores,
+                'Rol' => $Rol,
+                'View' => $View
             ]);
         }
         public static function actualizar(Router $router){
