b0f656f7bfb28e7846778fb309963b28fff06909
Rolando <rolando_osuna@live.com.mx>
2021-11-15 14:14:27 -0700

    Ajuste de table_usuario - Nueva table_producto

--- Views/Dashboard/section_usuario.php
+++ Views/Dashboard/section_usuario.php
@@ -2,7 +2,7 @@
     <h1 class="text-center">Usuario</h1>
     <?php if($Errores):?>
       <?php foreach($Errores as $Error):?>
-        <div class="text-start alert alert-danger d-flex bd-highlight " role="alert">
+        <div class="text-start alert alert-danger d-flex bd-highlight mx-5" role="alert">
           <div class="flex-grow-1 bd-highlight">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
               <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
@@ -76,7 +76,7 @@
       <div class="row ">
       <div class="col-12 text-center p-3">
         <input class="btn btn-primary" type="submit" value="Registrar">
-        <a class="btn btn-secondary" type="button" href="/Dashboard/dashboard?View=2">Volvér</a>
+        <a class="btn btn-secondary" type="button" href="/Dashboard/dashboard?View=2">Volver</a>
       </div>
     </div>
     </form>
