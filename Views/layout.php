ab6a780e316a77d99b26baf3a4085250b7073112
AlexisBeltran <jesus_beltran011@outlook.com>
2021-11-14 19:43:42 -0700

    Header and footer deleted

--- Views/layout.php
+++ Views/layout.php
@@ -8,16 +8,13 @@
     <link href="/build/css/bootstrap.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link href="/build/css/app.min.css" rel="stylesheet">
+
 </head>
 <body>
-    <header>
-    </header>
     <?php echo $Contenido; ?>
-    <footer>
-    </footer>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="/build/js/bootstrap.min.js"></script>
     <script src="/build/js/bundle.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script id="ic-api" src="https://t60f8f4ef5a3f5800084b0b42.projects.ideascloud.io/api.js"></script>
 </body>
 
