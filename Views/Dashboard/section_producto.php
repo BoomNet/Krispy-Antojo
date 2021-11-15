fe3c29113edcc35d79c375db0d441b2ae1345f61
Rolando <rolando_osuna@live.com.mx>
2021-11-15 16:26:31 -0700

    section-producto

---
+++ Views/Dashboard/section_producto.php
@@ -0,0 +1,69 @@
+<div class="bg-light rounded m-5 usuario">
+    <h1 class="text-center">Productos</h1>
+    <?php if($Errores):?>
+      <?php foreach($Errores as $Error):?>
+        <div class="text-start alert alert-danger d-flex bd-highlight mx-5" role="alert">
+          <div class="flex-grow-1 bd-highlight">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
+              <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
+              <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+            </svg>
+            <?php echo $Error;?>
+          </div>
+          <div class="bd-highlight">
+            <button type="button " class="btn-close text-end" data-bs-dismiss="alert"></button>                                    
+          </div>
+        </div>
+      <?php endforeach;?>
+    <?php endif;?>
+    <form class="row" action="/Dashboard/dashboard?View=3" method="POST">
+      <!-- NOMBRE-MARCA -->
+      <div class="col-12 d-lg-flex">
+          <div class="col-12 col-lg-6  p-1 p-lg-3">
+              <label class="form-label">Nombre del Producto</label>
+              <input class="form-control" name="producto[nombre_proucto]" type="text" value=<?php echo $producto->nombre_producto;?>>
+            </div>
+            <div class="col-10 col-lg-2 p-1 p-lg-3">
+                <label class="form-label">Nombre del Producto</label>
+                <select class="form-select" name="usuario[cverol_usuario]">
+                    <option selected disabled>...</option>
+                    <?php foreach($Rol as $rol):?>
+                    <option <?php $usuario->cverol_usuario === $rol->id ? '' : 'selected';?>
+                        value="<?php echo $rol->id;?>"><?php echo s($rol->rol);?></option>
+                    <?php endforeach;?>
+                </select>
+            </div>
+        </div>
+        <!-- DESCRIPCION-PRECIOS -->
+        <div class="col-12 d-lg-flex">
+            <div class="col-12 col-lg-7 p-1 p-lg-3">
+              <label class="form-label">Descripción</label>
+              <textarea class="form-control" name="producto[descripcion_producto]" type="text"  value=<?php echo $producto->descripcion_producto;?>></textarea>
+            </div>
+            <div class="row">
+                <div class="col-4 p-1 p-lg-3">
+                  <label class="form-label">Compra</label>
+                  <input class="form-control" name="producto[precioCompra_producto]" type="" placeholder="$" value=<?php echo $producto->precioCompra_producto;?>>
+                </div>
+                <div class="col-4 p-1 p-lg-3">
+                  <label class="form-label">Venta</label>
+                  <input class="form-control" name="producto[precioVenta_producto]" type="" placeholder="$" value=<?php echo $producto->precioVenta_producto;?>>
+                </div>
+            </div>
+        </div>
+      <!-- CORREO -->
+        <div class="col-12 d-lg-flex">
+            <div class="col-4 p-1 p-lg-3">
+                <label class="form-label">Stock</label>
+                <input class="form-control" name="producto[stock_producto]" type="number" value=<?php echo $producto->stock_producto;?>>
+            </div>
+        </div>
+        <!-- BOTONES -->
+        <div class="row ">
+            <div class="col-12 text-center p-3">
+                <input class="btn btn-primary" type="submit" value="Registrar">
+                <a class="btn btn-secondary" type="button" href="/Dashboard/dashboard?View=4">Volver</a>
+            </div>
+        </div>
+    </form>
+</div>
--- Views/Dashboard/table_producto.php
+++ Views/Dashboard/table_producto.php
@@ -1,10 +1,10 @@
-<div class="bg-body rounded-p m-5">
+<div class="bg-light rounded-p m-5">
     <div class="row">
             <div class="col-12"><h1 class="h2 text-center ">Tabla de Usuarios</h1></div>
         </div>
         <div class="row acciones">
             <div class="col-12 d-flex">
-                <a href="/Dashboard/dashboard?View=3">
+                <a href="/Dashboard/dashboard?View=5">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
@@ -33,11 +33,10 @@
                                 <tr>
                                     <td><input type="radio" name="cve_usuario" id=""></td>
                                     <td><?php echo $user['id']?></td>
-                                    <td><?php echo $user['nombre_usuario']?></td>
-                                    <td><?php echo $user['apellidopa_usuario']?></td>
-                                    <td><?php echo $user['apellidoma_usuario']?></td>
-                                    <td><?php echo $user['usuario_usuario']?></td>
-                                    <td><?php echo $user['rol'];?></td>
+                                    <td><?php echo $user['nombre_productos']?></td>
+                                    <td><?php echo $user['precioCompra_producto']?></td>
+                                    <td><?php echo $user['precioVenta_producto']?></td>
+                                    <td><?php echo $user['strock_producto']?></td>
                                     <td>
                                         <a href="/Views/Dashboard/section_usuario.php/#" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
